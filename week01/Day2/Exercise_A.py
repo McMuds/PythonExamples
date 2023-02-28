caf13f4e246f242308e854612895a0153c50c2e3
Claire <Claire@claires-mbp.home>
2023-02-28 20:06:48 +0000

    Exercises A & B initial commit

---
+++ week01/Day2/Exercise_A.py
@@ -0,0 +1,31 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+#1. Add "Edinburgh Waverley" to the end of the list
+stops.append("Edinburgh")
+print("1: ",stops)
+#2. Add "Glasgow Queen St" to the start of the list
+stops.insert(0,"Glasgow Queen St")
+print("2: ",stops)
+#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4,"Polmont")
+print("3: ",stops)
+#4. Print out the index position of "Linlithgow"
+print("4: ", stops.index("Linlithgow"))
+#5. Remove "Livingston" from the list using its name
+stops.remove("Livingston")
+print("5: ",stops)
+#6. Delete "Cumbernauld" from the list by index
+stops.remove(stops[2])
+print("6: ",stops)
+#7. Print the number of stops there are in the list
+print("7: ",len(stops))
+print("7b: ",stops)
+#8. Sort the list alphabetically
+stops.sort()
+print("8: ",stops)
+#9. Reverse the positions of the stops in the list
+stops.reverse()
+print("9: ",stops)
+#10 Print out all the stops using a for loop
+for stop in stops:
+    print("10: ",stop)
---
+++ week01/Day2/Exercise_B.py
@@ -0,0 +1,94 @@
+users = {
+  "Jonathan": {
+    "twitter": "jonnyt",
+    "lottery_numbers": [6, 12, 49, 33, 45, 20],
+    "home_town": "Stirling",
+    "pets": [
+    {
+      "name": "fluffy",
+      "species": "cat"
+    },
+    {
+      "name": "fido",
+      "species": "dog"
+    },
+    {
+      "name": "spike",
+      "species": "dog"
+    }
+  ]
+  },
+  "Erik": {
+    "twitter": "eriksf",
+    "lottery_numbers": [18, 34, 8, 11, 24],
+    "home_town": "Linlithgow",
+    "pets": [
+    {
+      "name": "nemo",
+      "species": "fish"
+    },
+    {
+      "name": "kevin",
+      "species": "fish"
+    },
+    {
+      "name": "spike",
+      "species": "dog"
+    },
+    {
+      "name": "rupert",
+      "species": "parrot"
+    }
+  ]
+  },
+  "Avril": {
+    "twitter": "bridgpally",
+    "lottery_numbers": [12, 14, 33, 38, 9, 25],
+    "home_town": "Dunbar",
+    "pets": [
+      {
+        "name": "monty",
+        "species": "snake"
+      }
+    ]
+  }
+}
+print("Starting:")
+# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print("1: " + users["Jonathan"] ["twitter"])
+# 2. Get Erik's hometown
+print("2: " + users ["Erik"] ["home_town"])
+# 3. Get the list of Erik's lottery numbers
+print("3a: " + str(users ["Erik"] ["lottery_numbers"]))
+print(f'3b: {users ["Erik"] ["lottery_numbers"]}')
+# 4. Get the species of Avril's pet Monty
+print("4: " + users ["Avril"] ["pets"] [0] ["species"])
+# 5. Get the smallest of Erik's lottery numbers
+users ["Erik"] ["lottery_numbers"].sort()
+print(f'5: {(users ["Erik"] ["lottery_numbers"] [0])}')
+# 6. Return an list of Avril's lottery numbers that are even
+# print(f'6: {str(users ["Avril"] ["lottery_numbers"]) }')
+
+# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+print(f'7a: {users ["Erik"] ["lottery_numbers"]}')
+users ["Erik"] ["lottery_numbers"].append(7)
+print(f'7b: {users ["Erik"] ["lottery_numbers"]}')
+# 8. Change Erik's hometown to Edinburgh
+print(f'8a: {users ["Erik"] ["home_town"]}')
+users ["Erik"] ["home_town"] = "Edinburgh"
+print(f'8b: {users ["Erik"] ["home_town"]}')
+# 9. Add a pet dog to Erik called "fluffy"
+print(f'9a: {users ["Erik"] ["pets"]}')
+users["Erik"] ["pets"].append({"name": "fluffy", "species": "dog"})
+print(f'9b: {users ["Erik"] ["pets"]}')
+# 10. Add another person to the users dictionary
+users["Claire"] = { 
+        "twitter": "no_ta",
+        "lottery_numbers": [4, 7, 33, 38, 15, 25],
+        "home_town": "Edinburgh",
+    "pets": [
+      {"name": "elsa", "species": "cat"},
+      {"name": "eddi", "species": "dog"}
+    ]
+    }
+print(f'10:  {users["Claire"]}')
