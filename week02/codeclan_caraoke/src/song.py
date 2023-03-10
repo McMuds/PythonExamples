19036a8cd9e0ca7d0a64813062e7e702b6b0bbf9
McMuds <clairemcmurdo@duck.com>
2023-03-10 12:07:36 +0000

    Test 2 - get song artist

--- week02/codeclan_caraoke/src/song.py
+++ week02/codeclan_caraoke/src/song.py
@@ -1,3 +1,6 @@
 class Song:
-    def __init__(self,song_name):
+    def __init__(self,song_name, artist,genre,singers):
         self.name = song_name
+        self.artist = artist
+        self.genre = genre 
+        self.singers = 0
--- week02/codeclan_caraoke/tests/song_test.py
+++ week02/codeclan_caraoke/tests/song_test.py
@@ -3,5 +3,12 @@ from src.song import *
 
 class TestSong(unittest.TestCase):
 
+    def setUp(self):
+        self.song1 = Song("Holiday","Madonna","pop",1)
+
     def test_song_has_name(self):
-        self.assertEqual("Holiday",Song("Holiday").name)
+        self.assertEqual("Holiday",self.song1.name)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Madonna",self.song1.artist)
+    
