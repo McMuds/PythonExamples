9630c691b6d19faa3be9550eb026dc84f75cef8c
McMuds <clairemcmurdo@duck.com>
2023-03-10 12:35:13 +0000

    Tested adding multiple people to a room at init

--- week02/codeclan_caraoke/tests/room_test.py
+++ week02/codeclan_caraoke/tests/room_test.py
@@ -6,6 +6,10 @@ class TestRoom(unittest.TestCase):
     
     def setUp(self):
         self.guest1 = Guest("Claire")
+        self.guest2 = Guest("Mar")
+        self.guest3 = Guest("Toby")
+        self.guest4 = Guest("Alex")
+        self.guest_list = [self.guest1, self.guest2, self.guest3]
         self.room1 = Room(1,self.guest1)
 
     def test_room_has_id(self):
@@ -18,3 +22,6 @@ class TestRoom(unittest.TestCase):
         empty_room = Room(2,[])
         self.assertEqual([],empty_room.guest)
 
+    def test_can_create_busy_room(self):
+        busy_room = Room(3,self.guest_list)
+        self.assertEqual(3,len(busy_room.guest))
