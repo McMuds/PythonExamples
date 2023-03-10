396add38843166542e5e494f87b951683b0d8fa2
McMuds <clairemcmurdo@duck.com>
2023-03-10 13:40:42 +0000

    tested removing guest from busy room

--- week02/codeclan_caraoke/tests/room_test.py
+++ week02/codeclan_caraoke/tests/room_test.py
@@ -14,6 +14,7 @@ class TestRoom(unittest.TestCase):
         self.song1 = Song("Wichita Lineman", "Glen Campbell", "Country", 1)
         self.room1 = Room(1,[self.guest1],[])
         self.empty_room = Room(2,[],[])
+        self.busy_room = Room(3,self.guest_list,[self.song1])
    
     def test_room_has_id(self):
         self.assertEqual(1,self.room1.id)
@@ -25,8 +26,7 @@ class TestRoom(unittest.TestCase):
         self.assertEqual([],self.empty_room.guests)
 
     def test_can_create_busy_room(self):
-        busy_room = Room(3,self.guest_list,[self.song1])
-        self.assertEqual(3,len(busy_room.guests))
+        self.assertEqual(3,len(self.busy_room.guests))
 
     def test_can_add_guest_to_room(self):
         self.room1.add_guest_to_room(self.guest4)
@@ -36,6 +36,11 @@ class TestRoom(unittest.TestCase):
         self.room1.add_song_to_room(self.song1)
         self.assertEqual(1,len(self.room1.songs))
 
-    def test_can_remove_guest_from_single_guest_room__pass(self):
+    def test_can_remove_guest_from_single_guest_room(self):
         self.room1.remove_guest(self.guest1)
         self.assertEqual([],self.room1.guests)
+
+    def test_can_remove_guest_from_busy_room(self):
+        self.busy_room.remove_guest(self.guest2)
+        expected = [self.guest1,self.guest3]
+        self.assertEqual(expected,self.busy_room.guests)
