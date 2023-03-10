280c7856e074f4110d33d75c98a9c8f398eb4682
McMuds <clairemcmurdo@duck.com>
2023-03-10 13:34:41 +0000

    Added songs to room

--- week02/codeclan_caraoke/src/room.py
+++ week02/codeclan_caraoke/src/room.py
@@ -1,7 +1,11 @@
 class Room:
-    def __init__(self,id,guests):
+    def __init__(self,id,guests, songs):
         self.id = id
         self.guests = guests
+        self.songs = songs
 
     def add_guest_to_room(self,new_guest):
         self.guests.append(new_guest)
+
+    def add_song_to_room(self,song):
+        self.songs.append(song)
--- week02/codeclan_caraoke/tests/room_test.py
+++ week02/codeclan_caraoke/tests/room_test.py
@@ -1,6 +1,7 @@
 import unittest
 from src.room import Room
 from src.guest import Guest
+from src.song import Song
 
 class TestRoom(unittest.TestCase):
     
@@ -10,9 +11,10 @@ class TestRoom(unittest.TestCase):
         self.guest3 = Guest("Toby")
         self.guest4 = Guest("Alex")
         self.guest_list = [self.guest1, self.guest2, self.guest3]
-        self.room1 = Room(1,[self.guest1])
-        self.empty_room = Room(2,[])
-
+        self.song1 = Song("Wichita Lineman", "Glen Campbell", "Country", 1)
+        self.room1 = Room(1,[self.guest1],[])
+        self.empty_room = Room(2,[],[])
+   
     def test_room_has_id(self):
         self.assertEqual(1,self.room1.id)
 
@@ -23,9 +25,13 @@ class TestRoom(unittest.TestCase):
         self.assertEqual([],self.empty_room.guests)
 
     def test_can_create_busy_room(self):
-        busy_room = Room(3,self.guest_list)
+        busy_room = Room(3,self.guest_list,[self.song1])
         self.assertEqual(3,len(busy_room.guests))
 
     def test_can_add_guest_to_room(self):
         self.room1.add_guest_to_room(self.guest4)
         self.assertEqual(2,len(self.room1.guests))
+
+    def test_can_add_song_to_room(self):
+        self.room1.add_song_to_room(self.song1)
+        self.assertEqual(1,len(self.room1.songs))
