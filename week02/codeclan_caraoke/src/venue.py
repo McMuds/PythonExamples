810bea360da14aa0d01bc463816ebdc4c14611b1
McMuds <clairemcmurdo@duck.com>
2023-03-11 09:34:37 +0000

    Able to add a room

--- week02/codeclan_caraoke/src/venue.py
+++ week02/codeclan_caraoke/src/venue.py
@@ -1,3 +1,9 @@
 class Venue:
-    def __init__(self,name):
+    
+    def __init__(self,name,till):
         self.name = name
+        self.till = till
+        self.rooms = [] #blank canvas - have to add rooms
+
+    def add_room(self,room_to_add):
+        self.rooms.append(room_to_add)    
--- week02/codeclan_caraoke/tests/venue_test.py
+++ week02/codeclan_caraoke/tests/venue_test.py
@@ -1,8 +1,19 @@
 import unittest
 from src.venue import Venue
+from src.room import Room
 
 class TestVenue(unittest.TestCase):
     
+    def setUp(self):
+        self.venue1 = Venue("Karaoke Karnage",100.0)
+        self.room1 = Room(1,[],[])
+
     def test_venue_has_name(self):
-        venue1 = Venue("Karaoke Karnage")
-        self.assertEqual("Karaoke Karnage",venue1.name)
+        self.assertEqual("Karaoke Karnage",self.venue1.name)
+
+    def test_venue_has_money(self):
+        self.assertEqual(100.0,self.venue1.till)
+
+    def test_venue_has_rooms(self):
+        self.venue1.add_room(self.room1)
+        self.assertEqual(1,len(self.venue1.rooms))
