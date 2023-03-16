b9b34558a6b74e10e9fd7c14c35ec6bceac5f120
McMuds <clairemcmurdo@duck.com>
2023-03-16 10:30:38 +0000

    Added {{title}} to the head

--- week03/day_03/flask_models_lab/controllers/controller.py
+++ week03/day_03/flask_models_lab/controllers/controller.py
@@ -5,13 +5,13 @@ from models.orders import order_list
 
 @app.route('/')
 def home():
-    return render_template("index.html", title='home', order_list=order_list)
+    return render_template("index.html", title="Mario's", order_list=order_list)
 
 @app.route('/orders')
 def index():
-    return render_template("index.html", title='home', order_list=order_list)
+    return render_template("index.html", title="Mario's", order_list=order_list)
 
 @app.route('/orders/<orderid>')
 def order(orderid):
     order_number = int(orderid)
-    return render_template("order.html", order=order_list[order_number])
+    return render_template("order.html", title="Mario's", order=order_list[order_number])
