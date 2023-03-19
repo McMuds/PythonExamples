cced7073fd085dfc4c99dc0be6153a2236066ec7
McMuds <clairemcmurdo@duck.com>
2023-03-19 20:51:39 +0000

    Added a little bit of responsiveness

--- week03/weekend_homework/models/books.py
+++ week03/weekend_homework/models/books.py
@@ -1,11 +1,14 @@
 from models.book import Book
 
+book1 = Book("To Kill a Mockingbird", "Harper Lee", "Fiction")
 book2 = Book("Zen and the Art of Motorcycle Maintenance",
              "Robert M. Pirsig", "self-help")
-book1 = Book("To Kill a Mockingbird", "Harper Lee", "Fiction")
 book3 = Book("The Girl Who Saved the King of Sweden", "Jonas Jonasson", "Fiction")
+book4 = Book("The Life of Pi","Yann Martel","Fiction")
+book5 = Book("The Princess Bride", "William Goldman", "Fiction")
+book6 = Book("The Food Lab", "Kenji Lopez-Alt", "Cookery")
 
-book_data = [book1, book2, book3]
+book_data = [book1, book2, book3, book5, book4, book6]
 
 def add_new_book(book):
     book_data.append(book)
