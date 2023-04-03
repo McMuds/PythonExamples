7740556f0f17eb9993eac570e2a399fe34c09251
McMuds <clairemcmurdo@duck.com>
2023-04-03 22:33:14 +0100

    Passed tests, code fixed

--- PDA/part_2_code/src/card_game.py
+++ PDA/part_2_code/src/card_game.py
@@ -4,26 +4,24 @@
 
 # Correct the errors below that you spotted in task 1.
 
+# from src.card import Card
 class CardGame:
 
-
   def check_for_ace(self, card):
-    if card.value = 1:
+    if card.value == 1:
       return True
-    else
+    else:
       return False
    
 
-  dif highest_card(self, card1 card2):
-  if card1.value > card2.value:
-    return card
-  else:
-    return card2
+  def highest_card(self, card1, card2):
+    if card1.value > card2.value:
+      return card1
+    else:
+      return card2
   
-
-
-def cards_total(self, cards):
-  total
-  for card in cards:
-    total += card.value
-    return "You have a total of" + total
+  def cards_total(self, cards):
+    total = 0
+    for card in cards:
+      total += card.value
+    return "You have a total of " + str(total)
